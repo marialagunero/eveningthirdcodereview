c7b5c4cdbf8e7623245fb898262626af92458c77
Maria Lagunero <maria58@gmail.com>
2019-12-21 22:28:35 -1000

    add change scripts. variable not returning value. Still working

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 //business logic
-var saying = function(number) {
+/*var saying = function(number) {
 
   if (number === 1) {
-    if (number.toString())
+    alert("Beep!");
+  } else {
+    alert ("Boop!");
   }
-}
+};*/
+
 
 
 //user interface logic
 $(document).ready(function() {
-  $("form#numberInput").submit(function(event) {
+  $("form#number").submit(function(event) {
     event.preventDefault();
 
-    var number = parseInt($("#numberInput")).val();
+    var number = parseInt($("#numberInput").val();
     var result = saying(number);
 
     $("#result").show();
